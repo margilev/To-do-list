7b6e52eb38449f1fbe6b53f11cb56f64305196c8
margalit <margilev@gmail.com>
2019-01-10 21:38:54 +0200

    i think it's done

--- script.js
+++ script.js
@@ -32,6 +32,10 @@ function creatList(task, date, hour) {
 }
 
 function mandatoryCheck(task, date) {
+    //checking date-validation//
+    var GivenDate = document.forms["main-list"]["main_input_date"].value;
+    var CurrentDate = new Date();
+    GivenDate = new Date(GivenDate);
 
     if (task == "") {
         alert("Please enter your task");
@@ -41,7 +45,10 @@ function mandatoryCheck(task, date) {
         alert("please fill in the date");
         return false;
     }
-    else {
+    //checking date-validation//
+    else if (GivenDate < CurrentDate) {
+        alert("The date has passed");
+    } else {
         return true;
     }
 }
@@ -82,7 +89,7 @@ function clearList() {
 
 function deletenote(id) {
 
-    var x = arr.findIndex(function (task) {
+    var x = arr.findIndex(function(task) {
         return task.id == id
     })
 
@@ -102,7 +109,7 @@ drawClock();
 setInterval(drawClock, 1000);
 
 function drawClock() {
-    ctx.arc(0, 0, radius, 0 , 2 * Math.PI);
+    ctx.arc(0, 0, radius, 0, 2 * Math.PI);
     ctx.fillStyle = "white";
     ctx.fill();
 }
@@ -111,71 +118,71 @@ function drawClock() {
     drawFace(ctx, radius);
     drawNumbers(ctx, radius);
     drawTime(ctx, radius);
-  }
-  
-  function drawFace(ctx, radius) {
+}
+
+function drawFace(ctx, radius) {
     var grad;
-  
+
     ctx.beginPath();
     ctx.arc(0, 0, radius, 0, 2 * Math.PI);
     ctx.fillStyle = 'white';
     ctx.fill();
-  
-    grad = ctx.createRadialGradient(0, 0 ,radius * 0.95, 0, 0, radius * 1.05);
+
+    grad = ctx.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
     grad.addColorStop(0, '#652001');
     grad.addColorStop(0.5, 'white');
     grad.addColorStop(1, '#652001');
     ctx.strokeStyle = grad;
-    ctx.lineWidth = radius*0.1;
+    ctx.lineWidth = radius * 0.1;
     ctx.stroke();
-  
+
     ctx.beginPath();
     ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
     ctx.fillStyle = '#652001';
     ctx.fill();
-  }
-  function drawNumbers(ctx, radius) {
+}
+function drawNumbers(ctx, radius) {
     var ang;
     var num;
     ctx.font = radius * 0.15 + "px arial";
     ctx.textBaseline = "middle";
     ctx.textAlign = "center";
-    for(num = 1; num < 13; num++){
-      ang = num * Math.PI / 6;
-      ctx.rotate(ang);
-      ctx.translate(0, -radius * 0.85);
-      ctx.rotate(-ang);
-      ctx.fillText(num.toString(), 0, 0);
-      ctx.rotate(ang);
-      ctx.translate(0, radius * 0.85);
-      ctx.rotate(-ang);
+    for (num = 1; num < 13; num++) {
+        ang = num * Math.PI / 6;
+        ctx.rotate(ang);
+        ctx.translate(0, -radius * 0.85);
+        ctx.rotate(-ang);
+        ctx.fillText(num.toString(), 0, 0);
+        ctx.rotate(ang);
+        ctx.translate(0, radius * 0.85);
+        ctx.rotate(-ang);
     }
-  }
-  function drawTime(ctx, radius){
+}
+function drawTime(ctx, radius) {
     var now = new Date();
     var hour = now.getHours();
     var minute = now.getMinutes();
     var second = now.getSeconds();
     //hour
-    hour = hour%12;
-    hour = (hour*Math.PI/6)+(minute*Math.PI/(6*60))+(second*Math.PI/(360*60));
-    drawHand(ctx, hour, radius*0.5, radius*0.07);
+    hour = hour % 12;
+    hour = (hour * Math.PI / 6) + (minute * Math.PI / (6 * 60)) + (second * Math.PI / (360 * 60));
+    drawHand(ctx, hour, radius * 0.5, radius * 0.07);
     //minute
-    minute = (minute*Math.PI/30)+(second*Math.PI/(30*60));
-    drawHand(ctx, minute, radius*0.8, radius*0.07);
+    minute = (minute * Math.PI / 30) + (second * Math.PI / (30 * 60));
+    drawHand(ctx, minute, radius * 0.8, radius * 0.07);
     // second
-    second = (second*Math.PI/30);
-    drawHand(ctx, second, radius*0.9, radius*0.02);
-  }
-  
-  function drawHand(ctx, pos, length, width) {
+    second = (second * Math.PI / 30);
+    drawHand(ctx, second, radius * 0.9, radius * 0.02);
+}
+
+function drawHand(ctx, pos, length, width) {
     ctx.beginPath();
     ctx.lineWidth = width;
     ctx.lineCap = "round";
-    ctx.moveTo(0,0);
+    ctx.moveTo(0, 0);
     ctx.rotate(pos);
     ctx.lineTo(0, -length);
     ctx.stroke();
     ctx.rotate(-pos);
-  }
+}
   //canvas clock end//
